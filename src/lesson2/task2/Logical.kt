d9e0f193d73b7281c05c3778c315fb913d8e0008
Imboredik <imboredik@gmail.com>
2022-09-16 10:30:47 +0300

    1.4

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -52,27 +52,11 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    var (jan: Int, mar: Int, may: Int, jul: Int, aug: Int) = List(5) { 31 }
-    var (apr: Int, jun: Int, sen: Int, nov: Int) = List(4) { 30 }
-    var (oct: Int, dec: Int) = List(2) { 31 }
-    var feb = 28
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400  == 0) feb = 29
-    return when (month) {
-        1 -> jan
-        2 -> feb
-        3 -> mar
-        4 -> apr
-        5 -> may
-        6 -> jun
-        7 -> jul
-        8 -> aug
-        9 -> sen
-        10 -> oct
-        11 -> nov
-        12 -> dec
-        else -> TODO()
-    }
+    val b = mutableListOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31)
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) b[1] = 29
+    return b[month - 1]
 }
+
 
 /**
  * Простая (2 балла)
