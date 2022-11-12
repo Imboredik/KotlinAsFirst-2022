469c9c6ac56a73756b0b8eecbf6ff32e6956aa48
Imboredik <imboredik@gmail.com>
2022-11-12 23:49:40 +0300

    1.3

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -294,7 +294,19 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    val numb: Pair<Int, Int> = Pair(-1, -1)
+    if (list.isNotEmpty()) {
+        for (i in list.indices) {
+            for (j in i + 1..list.lastIndex) {
+                if (list[i] + list[j] == number) {
+                    return i to j
+                }
+            }
+        }
+    }
+    return numb
+}
 
 /**
  * Очень сложная (8 баллов)
